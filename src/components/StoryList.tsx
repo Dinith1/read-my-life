9450f9abc889d75809a7292ce302cfc2e90d90d8
Dinith Wannigama <dinith1@github.com>
2018-11-19 21:00:09 +1300

    added some buttons

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -3,19 +3,27 @@ import './StoryList.css';
 
 interface IProps {
     stories: any[],
+    read: any,
+    searchTitle: any,
+    searchAuthor: any,
 }
 
 export default class StoryList extends React.Component<IProps, {}> {
     constructor(props: any) {
-        super(props);
+        super(props)
+        this.read = this.read.bind(this)
+        this.searchTitle = this.searchTitle.bind(this)
+        this.searchAuthor = this.searchAuthor.bind(this)
     }
 
     public render() {
         return (
             <div>
                 <div className="search-bar">
-                    <input className="search-input" type="text" placeholder="Search for a story" />
-                    <button className="search-button"> Search </button>
+                    <input className="search-input" id="search-input" type="text" placeholder="Search for a story by title or author" />
+                    <button className="search-button" onClick={this.searchTitle}> Search By Title </button>
+                    <button className="search-button" onClick={this.searchAuthor}> Search By Author </button>
+
                 </div>
 
                 <div className="story-table">
@@ -51,10 +59,45 @@ export default class StoryList extends React.Component<IProps, {}> {
             children.push(<td key={"author" + i}> {story.author} </td>)
             children.push(<td key={"description" + i}> {story.description} </td>)
             children.push(<td key={"rating" + i}> {story.rating} </td>)
-            children.push(<td key={"read-button" + i}><button> READ </button></td>)
+            children.push(<td key={"read-button" + i}><button onClick={this.read}> READ </button></td>)
             table.push(<tr key={i + ""} id={i + ""}>{children}</tr>)
         }
         return table
+    }
+
+    private read() {
+        alert("READING");
+    }
+
+    private searchTitle() {
+        const userInput = document.getElementById("search-input") as HTMLInputElement
+        if (this.checkInputIsEmpty(userInput)) {
+            return;
+        }
+        const searchString = userInput.value
+        alert("SEARCHED FOR TITLE : " + searchString)
+
+        // Do a READ request with the given title
+    }
+
+    private searchAuthor() {
+        const userInput = document.getElementById("search-input") as HTMLInputElement
+        if (this.checkInputIsEmpty(userInput)) {
+            return;
+        }
+        const searchString = userInput.value
+        alert("SEARCHED FOR AUTHOR : " + searchString)
+
+        // Do a READ request with the given author
+    }
+
+    // Method to check input isn't empty
+    private checkInputIsEmpty(userInput: any) {
+        if ((userInput === null) || (userInput.value === null) || (userInput.value === "")) {
+            alert("FAIL")
+            return true;
+        }
+        return false;
     }
 
 }
