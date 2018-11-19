10c99b2e8ec2a6701f98adc0a07261008b59db29
Dinith Wannigama <dinith1@github.com>
2018-11-19 18:33:50 +1300

    scrollable table

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import './StoryList.css';
 
 interface IProps {
     stories: any[],
@@ -13,18 +13,31 @@ export default class StoryList extends React.Component<IProps, {}> {
     public render() {
         return (
             <div>
-                <table className="story-table">
-                    <tbody>
-                        {this.createTable()}
-                    </tbody>
-                </table>
+                <div className="search-bar">
+                    <input className="search-input" type="text" placeholder="Search for a story" />
+                    <button className="search-button"> Search </button>
+                </div>
+
+                <div className="story-table">
+                    <table className="story-table">
+                        <tbody>
+                            <tr>
+                                <th>title</th>
+                                <th>Author</th>
+                                <th>Description</th>
+                                <th>Rating</th>
+                            </tr>
+                            {this.createTable()}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
 
     // Fill the table with story data
     private createTable() {
-        const table:any[] = []
+        const table: any[] = []
         const storyList = this.props.stories
 
         if (storyList == null) {
@@ -34,11 +47,12 @@ export default class StoryList extends React.Component<IProps, {}> {
         for (let i = 0; i < storyList.length; i++) {
             const children = []
             const story = storyList[i][0]
-            children.push(<td key={"title" + i}>{story.title}</td>)
-            children.push(<td key={"author" + i}>{story.author}</td>)
-            children.push(<td key={"description" + i}>{story.description}</td>)
+            children.push(<td key={"title" + i}> {story.title} </td>)
+            children.push(<td key={"author" + i}> {story.author} </td>)
+            children.push(<td key={"description" + i}> {story.description} </td>)
+            children.push(<td key={"rating" + i}> {story.rating} </td>)
             children.push(<td key={"read-button" + i}><button> READ </button></td>)
-            table.push(<tr key={i+""} id={i+""}>{children}</tr>)
+            table.push(<tr key={i + ""} id={i + ""}>{children}</tr>)
         }
         return table
     }
