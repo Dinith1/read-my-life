9a1b1e1bbd746927b460b77a6132be74688e230d
Dinith Wannigama <dinith1@github.com>
2018-11-23 13:31:08 +1300

    create modal resized

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Modal from 'react-responsive-modal';
 import './App.css';
 import ControlBar from './components/ControlBar';
 import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -82,7 +81,7 @@ export default class App extends React.Component<{}, IState> {
           <div className="login">
             <Button bsStyle="warning" onClick={this.openFaceLogin}>Developer Login</Button>
             {(this.state.devAuthenticated) ? <div className="logged-in" id="logged-in">Logged in as Developer</div> : ""}
-            <Modal open={this.state.devLogin} onClose={this.closeFaceLogin} closeOnOverlayClick={false} showCloseIcon={false} center={true}>
+            <Modal open={this.state.devLogin} onClose={this.closeFaceLogin} closeOnOverlayClick={false} showCloseIcon={true} center={true}>
               <Webcam
                 audio={false}
                 screenshotFormat="image/jpeg"
@@ -116,23 +115,23 @@ export default class App extends React.Component<{}, IState> {
           <form>
             <div className="form-group">
               <label>Title</label>
-              <input type="text" className="form-control-title" id="story-title-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.title : ""} placeholder="Enter Title" maxLength={25} />
+              <input type="text" className="form-control-title" id="story-title-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.title : ""} placeholder="Enter Title (max 25 characters)" maxLength={25} />
             </div>
 
             <div className="form-group">
               <label>Author</label>
-              <input type="text" className="form-control-author" id="story-author-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.authorName : ""} placeholder="Enter your name" maxLength={25} />
+              <input type="text" className="form-control-author" id="story-author-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.authorName : ""} placeholder="Enter your name (max 25 characters)" maxLength={25} />
             </div>
 
             <div className="form-group">
               <label>Description</label>
-              <input type="text" className="form-control-description" id="story-description-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.description : ""} placeholder="Enter a short description" maxLength={30} />
+              <textarea className="form-control-description" id="story-description-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.description : ""} placeholder="Enter a short description (max 30 characters)" maxLength={30} />
             </div>
 
             <div className="form-group">
               <FormControl required className="form-control" id="story-tag-input">
-                <InputLabel htmlFor="tag-required">Tag</InputLabel>
-                <Select id="tag-select" className="form-control" value={this.state.createTag} name="createTag" inputProps={{ id: 'tag-required', }} onChange={(evt) => { this.setState({ createTag: evt.target.value }) }}>
+                <InputLabel className="form-control-tag" htmlFor="tag-required">Tag</InputLabel>
+                <Select id="tag-select" className="form-control-tag" value={this.state.createTag} name="createTag" inputProps={{ id: 'tag-required', }} onChange={(evt) => { this.setState({ createTag: evt.target.value }) }}>
                   <MenuItem value="funny">Funny</MenuItem>
                   <MenuItem value="happy">Happy</MenuItem>
                   <MenuItem value="love">Love</MenuItem>
@@ -140,13 +139,12 @@ export default class App extends React.Component<{}, IState> {
                   <MenuItem value="scary">Scary</MenuItem>
                   <MenuItem value="strange">Strange</MenuItem>
                 </Select>
-                <FormHelperText>Required</FormHelperText>
               </FormControl>
             </div>
 
             <div className="form-group">
               <label>Story</label>
-              <input type="text" className="form-control-contents" id="story-contents-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.contents : ""} maxLength={500} placeholder="Write your story (max length = 500 characters)" />
+              <textarea className="form-control-contents" id="story-contents-input" defaultValue={(this.state.isEdit) ? this.state.storyToRead.contents : ""} maxLength={500} placeholder="Write your story (max 500 characters)" />
             </div>
 
             <button type="submit" className="btn" onClick={this.confirmForm}>Publish</button>
